d16c75bc259b2b76bd298c3fe4bafa7d6a61924a
zlb <1052519137@qq.com>
2022-10-16 23:04:53 +0800

    框架搭建完成

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    WindiCSS()
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, 'src'),
+    }
+  }
 })
