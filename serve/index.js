5eedbf411d3b7811b083c8766abfc1bec5d9a154
zlb <1052519137@qq.com>
2022-10-17 00:31:25 +0800

    添加接口

--- serve/index.js
+++ serve/index.js
@@ -3,35 +3,76 @@ const require = createRequire(import.meta.url);
 let http = require('http')
 let server = http.createServer()
 
+
+
+function createData (length, title, desc) {
+  return Array.from({ length: length }).map((it, index) => {
+    return {
+      id: index + 1,
+      icon: '',
+      title: title + index + 1,
+      desc: desc + '.....',
+    }
+  })
+}
+
+function getSystemList () {
+    return {
+      code: 200,
+      data: createData(10, '系统', '描述.....'),
+      headerData: {
+        icon: '',
+        title: '',
+        subTile: '',
+        name: ''
+      }
+    }
+}
+
+function getAnthConfigList () {
+    return {
+      code: 200,
+      data: createData(100, '配置参数', '配置参数.....')
+    }
+}
+
+function getNetworkList () {
+    return {
+      code: 200,
+      data: createData(50, '网络设置', '网络设置.....')
+    }
+}
+
+function getIndividuationList () {
+    return {
+      code: 200,
+      data: createData(30, '个性化设置', '个性化设置.....')
+    }
+}
+
+function getApplicationList () {
+    return {
+      code: 200,
+      data: createData(50, '应用列表', '应用列表.....')
+    }
+}
+  
+const publicMap = {
+  '/getSystemList': getSystemList,
+  '/getAnthConfigList': getAnthConfigList,
+  '/getNetworkList': getNetworkList,
+  '/getIndividuationList': getIndividuationList,
+  '/getApplicationList': getApplicationList,
+}
+
+
 server.on('request',function (request,response) {
-    response.write('<head><meta charset="utf-8"/></head>')   //处理中文乱码
-    let url = request.url //端口号后面的那一部分
-    //根据url响应不同的页面（给ta一个回复--->字符串）
-    if(url ==='/'){
-        response.end('这是index页面')
-    }else if (url === '/login.html'){
-        response.end('这是用户的登录页面')
-    }else if (url === '/register.html'){
-        response.end('这是用户的注册页面')
-    }else if(url === '/ding.json'){
-        //也可以给ta一个json
-        let ding = [
-            {
-                name:'dingqiusi',
-                age:19
-            },
-            {
-                name:'dingqisui2',
-                age:18
-            },
-            {
-                name: 'dingqisui3',
-                age: 20
-            }
-        ]
-        //然后把这个json转成字符串 最后ta要的格式是 string或者buffer
-        response.end(JSON.stringify(ding))
-    }else {
+    response.write('<head><meta charset="utf-8"/></head>')
+    let url = request.url 
+    const getter = publicMap[ url ]
+    if (getter) {
+        response.end(JSON.stringify(getter()))
+    } else {
         response.end('404 Not Found')
     }
 })
